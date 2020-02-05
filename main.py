ee5fc57879b660a1e6482fc9fd63b2b4b4a1edf3
Nikita <60404032+nglevc39@users.noreply.github.com>
2020-02-05 14:33:27 -0500

    Programming Story

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-# A starter program for Python with Tkinter
+
 
 from tkinter import * # import Tkinter library
 window = Tk()         # Create the application window
 
 # Add a label with the text "Hello"
-lbl = Label(window, text="Hello")
-
-# Place the label in the window at 0, 0
+lbl = Label(window, text=0, font=("Arial Bold", 50))
 lbl.grid(column=0, row=0)
+
+score = 0
+
+def addToScore():
+  global score
+  score += 1
+  lbl['text'] = score
+
+btn = Button(window, text="Click", command=addToScore)
+btn.grid(column = 0 , row = 1)
+
+btn = Button(window, text="Click", command=addToScore)
+btn.grid(column = 1 , row = 2)
+
+btn = Button(window, text="Click", command=addToScore)
+btn.grid(column = 2 , row = 3)
+
 
 
 window.mainloop()     # Keep the window open
---
+++ second.py/third.py
@@ -0,0 +1,40 @@
+def clear(): 
+    from os import system, name 
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
+
+    # for mac and linux(here, os.name is 'posix') 
+    else: 
+        _ = system('clear')
+clear()
+
+answer1 = input("Welcome to The Invasion, would you like to get started?.")
+
+if answer1 == "No":
+  print(" Why are you here.")
+  
+else:
+  print("Welcome, you will play as Sergeant Gleb. You are part of the United Nations Space Command defending against the invading Aliens called the Covenant. The Covenant is made up of different species of aliens. Grunts= Canon Fodder. Elites= Your equals. Jackals=snipers.  ")
+print("You are stationed at Outpost Delta 1, you need to use the bathroom but your squad needs to be sent out for a patrol.")
+choice1 = input("Do you use the bathroom or go out with your squad? Choose 1 or 2.")
+if choice1 == "1":
+choice2=input("You go to use the bathroom and tell your squad to wait for you, you reach the bathroom. Should you use the 1st uniral or the 2nd one?")
+if choice2 == "1st"
+choice3=input("you use the 1st uniral, you empty your bladder. you zip up and go wash to wash your hands. Right as you were about to wash your hands something rips through the cieling and crashes into the 2nd uniral.You see the strange object is a plasma ball, used by the covenant. Should you run out(first) or examine it closer.?(second)")
+if choice3 == "first"
+choice4=input("You run out of the bathroom, you and make it outside, you see Covenant ships floating over your head, and dropping troops. Your squad is no where insight, should you flee the action(1) or get closer to the enemy troops?(2)")
+if choice4 == "2"
+choice5=input("You get closer to the troops, the firing starts to get more intense the closer you get. You see a pack of grunts and a Elite leading them. Should you ignore them(1) or open fire(2).
+if choice5 == "1"
+choice6=("You ignore the aliens and run around them. You start to quickly realize that theres a UNSC chopper evacuating soldiers, maybe your squad is on there. Should you hop on(1) or stay and keep looking?(2) ")
+
+
+
+
+
+
+
+
+
+-")
