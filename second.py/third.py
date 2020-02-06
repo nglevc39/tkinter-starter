2e8456058e0d7d494120be80926d22d8287834c0
Nikita <60404032+nglevc39@users.noreply.github.com>
2020-02-06 14:33:27 -0500

    Update third.py

--- second.py/third.py
+++ second.py/third.py
@@ -8,7 +8,6 @@ def clear():
     else: 
         _ = system('clear')
 clear()
-
 answer1 = input("Welcome to The Invasion, would you like to get started?.")
 
 if answer1 == "No":
@@ -19,7 +18,7 @@ else:
 print("You are stationed at Outpost Delta 1, you need to use the bathroom but your squad needs to be sent out for a patrol.")
 choice1 = input("Do you use the bathroom or go out with your squad? Choose 1 or 2.")
 if choice1 == "1":
-choice2=input("You go to use the bathroom and tell your squad to wait for you, you reach the bathroom. Should you use the 1st uniral or the 2nd one?")
+choice2=input("You go to use the bathroom and tell your squad to wait for you, you reach the bathroom.Should you use the 1st uniral or the 2nd one?")
 if choice2 == "1st"
 choice3=input("you use the 1st uniral, you empty your bladder. you zip up and go wash to wash your hands. Right as you were about to wash your hands something rips through the cieling and crashes into the 2nd uniral.You see the strange object is a plasma ball, used by the covenant. Should you run out(first) or examine it closer.?(second)")
 if choice3 == "first"
@@ -28,6 +27,68 @@ if choice4 == "2"
 choice5=input("You get closer to the troops, the firing starts to get more intense the closer you get. You see a pack of grunts and a Elite leading them. Should you ignore them(1) or open fire(2).
 if choice5 == "1"
 choice6=("You ignore the aliens and run around them. You start to quickly realize that theres a UNSC chopper evacuating soldiers, maybe your squad is on there. Should you hop on(1) or stay and keep looking?(2) ")
+if choice6 == "1"
+choice7=("You run to hop on the helicopter but your squad hops out and one of them says, Sir sorry for leaving you, but we are here now! You were about to say something but a covenant speder rams into you. A squad of Grunts and a Elite leading them charges you. You fall back but your squad is still fighting, should you go back in there or find a weapon?(2)")
+if choice7 == "2"
+choice8=("You run to find a better weapon, you see a plasma rifle on the floor, you figure out how to use it. You go back and open fire on the Grunts and they soon fall back. You and your squad regroup and you see another dropship drop off a Covenant General. He drops down with his troops, Both sides engage. You and the General engage in 1v1 combat, he whips out his energy sword. You have a combat knife, Should you run up and go for a attack?(1) Or Wait for him(2)")
+print("░░░░░░░░░░░░░░░░░░░░░░░▄▄██▀▀░░░░░░▀███░░░░░░░░░░░░░░░░░░░░░░░░░░▄█░▐░░░░░░░░")
+print("░░░░░░░░░░░░░░░░░░░░░░░███░░░░░░█░░░░░▀██▄░░░░░░░░░░░░░░░░░░░░░▄█▄▀░█░░░░░░░░:)
+print("░░░░░░░░░░░░░░░░░░░░░▐██░░░░░░░░█░░░░░░░████░░░░░░░░░░░░░░░░▄▀▀▀▀█░░░█▀█░░░░░")
+print("░░░░░░░░░░░░░░░░░░░░░██░░░░░░░░░█░░░░▐▄█░██▌░░░░░░░░░░░░░░░█░░░░░░█▄█░░▐░░░░░")
+print("░░░░░░░░░░░░░░░░░░░░░███░░░░░█▄▄█▄▄░░░██▄░██░░░░░░░░░░░░░░█░░░░░▄█░░░▀▄█░░░░░")
+print("░░░░░░░░░░░░░░░░░░░░░███▀▀▀▀▀▀░░█▄░▀▀███▄▄██░░░░░░░░░░░░░░▌░░░░█░░░░░▐▌█▀█░░░")
+print("░░░░░░░░░░░░░░░░░░░░░██░░▄▄█░░░▐▄█░░░▄▄▀█░░█▌░░░░░░░░░░░░░░░░░▐░░░░░░░▌▐░░▌░░")
+print("░░░░░░░░░░░░░░░░░░░░░█▌▐█░░░▀█░░▄▄█▀░▄██▄░░▐▌░░░░░░░░░░░░░▌░░░▐░▄██▄░░▌▐░░▌░░")
+print("░░░░░░░░░░░░░░░░░░░░░█▌▐░░▄░░░▀▀░░░░▄▐█▀██░▐▌░░░░░░░░░░░░░██▀███▀▄░░▄█░▐░░▌░░")
+print("░░░░░░░░░░░░░░░░░░░░░▐░▐░▐██▄░░░░░▄░████▄▌░▐▌░░░░░░░░░░░░░▐█████▄░█▀█░█░██░░░")
+print("░░░░░░░░░░░░░░░░░░░░░░█░▌░▀▀█▌░░░░░███▀░░█░█░░░░░░░░░░░░░░░▀█▄▐░███░█░░▌▀░░░░")
+print("░░░░░░░░░░░░░░░░░░░░░░▐█▌░░░░░░░▄▀██░▀░░░▄▀░░░░░░░░░░░░░░░░░░░█▀▀▀█░█░░▌░░░░░")
+print("░░░░░░░░░░░░░░░░░░░░░░█░▀█▄░█▀█▀▀▀██▄▄░█▀██▄░░░░░░░░░░░░░░░░░░▐░░░▐░▀██░░░░░░")
+print("░░░░░░░░░░░░░░░░░▄▄▄█▀█░░░░██░█▄░█▄█░██░░░█░░▀█▄▄░░░░░░░░░░░░░▀▀█▄█▀░░░░░░░░░")
+print("░░░░░░░░░░░▄▄█▀▀░░░░░░██▄▄▄██▀░░▀░░░█▀░░░░█░░░░░░▀█▄░░░░░░░░░▀░░░░░▀█░░░░░░░░")
+print("░░░░░░░░▄▀░░░░░░░░░░░░█░░░░░░█░░░░░█▀▀▀▀▀▀░▌░░░░░░░░▀█▄░░░░░▐░░░░░░░▐░░░░░░░░")
+print("░░░░░░░█░░░░░░░░░░░░░░░▐█▄▄▄▄█░░░░░█░░░░▄▄▀░░░░░░░░░░░░░█▄░░▌░░░░░░░▐░░░░░░░░")
+print("░░░░░░█▄▄▄▄▄▄▄▄▄▄▄▄▄█▀▀▀░█░░░░░░░░░░▀██▀░░░░░░░░░░░░░░░░░░█▐░░░░░░░░█░░░░░░░░")
+print("░░░░░░██▄▄▄▄▄█▀▀▀█▀▀▐░▄▄░▐▄▄▄▄▄▄▄▄▄▄█▐█▀█▀█▀▀▀▀▀▀▀▀▀▀████▀▄▌░░░░░░░░░░░░░░░░░")
+print("░░░░░▐░░░░░░░░░░░▐░░▐░░░░▐░░░░░░░░░░▐░░░█░▌░░░░░░░░░░░░░▀▀█▄░░░░░░░█░░░░░░░░░")
+print("░░░░░█░░░░░█░░░░░█░░▐▄▄▄▄█▀▀▀▀▀▌░░░░▐▄▄█░░█░░░░░░░░░░░░░░░░░▀█░░░░█░░░░░░░░░░")
+print("░░░░█░░░░░▐░░░░░░█▀▀▄▄▄▄▌░░░░░░█▄▄▄▄▄▄▄▄▄▀▐▀█▄▄░░░░░░░░░░░░░▄▀░░░█░░░░░░░░░░░")
+print("░░░█░░░░░░▌░░░░░░█▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░░░░░░░█░░░░▀▀▀▄▄▄▄▄▄▄▄█▀█▄▄▄█░░░░░░░░░░░░")
+print("░░░▌░░░░░█░░░░░░▐░░░░░░░░░░░░░▀▀▀▀▀▀▀▀▀▀░░▌░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+print("░░░▌░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+
+if choice8 == "2"
+choice9=input("You wait for him to make a move, he sprints at you and swings, you duck and stab him in the chest. He groans and steps back. Should you take a swing at him?(1) or Wait for him to make a move?(2)")
+choice9 == "2"
+choice10=input("You wait for him to make a move, he waits for you to make a move....He gets impatient and charges you with a screem. He stops right in front of you and dosen't move. Should you attack him(1) or stay still.(2)")
+choice10 == "2"
+choice11=input("You wait and stay still, He dosen't make a move...He gets impatient and yells.You spot a Magnum on the floor, Should you go for it(1), or attack him?")
+choice11 == "1"
+choice12=input("You go for the magnum and he chaces after you, You grab the magnum and turn around. You open fire and blast his head open.")
+print("░▒▒▒▒▒░░░░░░░░░░░░░░░░░▄▄██▀▀░░░░░░▀███░░░░░░░░░░░░░░░░▄▄░░░░░░░░░░░░░░")
+print("░░░░░▒▒▒▒░░░░░░░░░░░░░░███░░░░░░█░░░░░▀██▄░░░░░▄▄▄████▀▀░░░░░░░░░░░░░░░")
+print("░░░░░░░░▒▒▒░░░░░░░░░░▐██░░░░░░░░█░░░░░░░████▀██▄█▀▀░░░░░░░░░░░░░░░░░░░░")
+print("░░░░░░░░░░░▒▒▒▀▀░░░░░██░░░░░░░░░█░░░░▐██▄███▀░░░░░░░░░░░░░░░░░░░░░░░░░░")
+print("░░░░░░░░░░░░░▒▒▒░░░░░███░░░░░█▄▄█▄▄▄████▄▄██▄▄▄▄▄▄▄▄░░░░░░░░░░░░█▌░░░░░")
+print("░░░░░░░░░░░░██▄░▒▒▒▀████▀▀▀▀▀▀░▄░██▀▀███▄▄██▄▄▄█▀▀██▄░░░░░░░░░░░░██░░░░")
+print("░░░░░░░░░░░███░░░░▒▒▒████▄▄█░▐▒▒█▒▒▒█████▄▄██▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░░░")
+print("░░░░░░░░░░████░░░░░░▒██████▄▄▄▄▄▒▄█▀█▄███▀▀███░░░░░░█░░░░░░░░░░▀░░░░░░░")
+print("░░░░░░░░░░█████░░░░░░██▒█▄▒▒▒▒███▌▒▒▒████████░░░░░░░███▄░░░░░░░░░░░░░░░")
+print("░░░░░░░░░░░▀▀▀▀░░░░░░█▄▒▒▒▒██▀▒█▀▀█▒▒█████▄██▌░░░░░░▀▀░░░░░░░░░░░░░░░░░")
+print("░░░░░░░░░░░░░░░░░▄▄██████▒▒▒███▒▒▒▒███▀░░█▀█▀███▄▄▄░░░░░░░░░░░░░░░░░░░░")
+print("░░░░░░░▄▄█▀▀▀███████▀▀███▒▒███▒███▀▀▀▀░░░▄▀░░░░░██████▄░░░░░░░░▀▀█░░░░░")
+print("░░░░░░░░░░░░░░▀▀▀▀▀▀▀▀█▀▀████▒▒██▀███▄░█▀██▄░░░░░░░░░░▀▀░░░░░░░░░░░░░░░")
+print("░░██░░░░░░░░░░░░░▄▄▄█▀█░░░░██▄████▄█▌██░░░█░░▀█▄▄░░░░░░░░░░░░░░░░░░░░░░")
+print("░░██░░░░░░░▄▄█▀▀░░░░░░███▄▄████▀▀░░░█░░░░▄█░░░░░░▀█▄░░░░░░░░░░░░░░░░░░░")
+print("░░░░░░░░▄▀░░░░░░░░░▄▄████▀████▌░░░▐█░▀▀▀▀░█░░░░░░░░░▀█▄░░░░░░░░░░░░░░░░")
+print("░░░░░░░█░░░░░░░▄███▀████░█████░░░░░▀▀▀▀▀▀▀░░░░░░░░░░░░░░█▄░░░░░░░░░░░░░")
+print("░░░░░░█▄▄▄▄▄▄███▄████▀█▀░██░░▀█▄▄░░░▀██▀░░░░░░░░░░░░░░░░░░█░░░░░░░░░░░░")
+print("░░░░░░██▄▄▄▄▄█▀▀▀█▀▀▐░████▄▄▄▄███▄▄▄█▐█▀█▀█▀▀▀▀▀▀▀▀█▀████▀░░░░░░░░░░░░░")
+print("░░░░░▐░░░░░░░░░░░▐░░▐███░▐░░░██░░░░░▐░░░█░▌░░░░░░░░░▄░░░░░░░░░░░░░░░░░░")
+print("░░░░░█░░░░░░░░░░░█░░██▄▄▄█▀▀▀▀▀█░░░░▐▄▄█░░█░░░░░░░░░▐░░░░░░░░░░░░░░░░░░")
+print("░░░░█░░░░░░░░░░░░█▀██▄▄▄▌░░░░░░█▄▄▄▄▄▄▄▄▄▀▐▀█▄▄▄▄▄▄░░▌░░░░░░░░░░░░░░░░░")
+print("░░░█░░░░░░░░░░░░░█▄█▄▄▄▄▄▄▄▄▄▄░░▀░░░░░░░█▀█▀▀▀░▀░░░░█░░░░░░░░░░░░░░░░░░")
+")
 
 
 
@@ -35,6 +96,3 @@ choice6=("You ignore the aliens and run around them. You start to quickly realiz
 
 
 
-
-
--")
